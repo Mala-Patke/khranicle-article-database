176c96785b305820f19015218c5d81dac8d4648c
Mala Patke <alis@khanlabschool.org>
2022-12-19 22:58:36 -0800

    Kill axios and remove unused deps

--- pages/api/getArticleData.js
+++ pages/api/getArticleData.js
@@ -1,12 +1,13 @@
-const { get } = require('axios').default;
 const attrEnum = ["title", "author", "beat", "desc", "edition", "date", "url"];
+
 /**
  * @param {import("http").IncomingMessage} req
  * @param {import("http").ServerResponse} res
  */
 export default async function handler(req, res) {
-    const { data } = await get(process.env.GOOGLE_SHEET_URL);
-    
+    const request = await fetch(process.env.GOOGLE_SHEET_URL);
+    const data = await request.json();
+
     let sheetData = data.sheets[0].data[0].rowData
         .map(e => e.values.map(ee => ee.formattedValue))
         .filter(e => e[0])
