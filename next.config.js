129c61b95ff69846d66d2c8e1a84ece0b68dc1b9
Mala Patke <alis@khanlabschool.org>
2022-12-20 14:46:55 -0800

    remove rewrites

--- next.config.js
+++ next.config.js
@@ -3,28 +3,6 @@ const nextConfig = {
     reactStrictMode: true,
     swcMinify: true,
 
-    async rewrites() {
-        return [
-            {
-                source: '/api/:path*',
-                destination: '/:path*',
-            },
-        ]
-    },
-
-    async headers() {
-        return [
-            {
-                source: "/api/:path*",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "https://khranicle.com" },
-                    { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-                ]
-            }
-        ]
-    }
     
 };
 
