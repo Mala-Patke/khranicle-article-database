6d18c6360651a79e9b1e3a20efb40ad1900abe5d
Mala Patke <alis@khanlabschool.org>
2022-12-20 17:05:47 -0800

    Update headers

--- next.config.js
+++ next.config.js
@@ -4,13 +4,12 @@ const nextConfig = {
     swcMinify: true,
 
     async headers() {
-        console.log("I've been called in next.config.js!");
         return [
             {
                 source: "/api/getArticleData",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "https://khranicle.com" },
+                    { key: "Access-Control-Allow-Origin", value: "https://www.khranicle.com/" },
                     { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
                     { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
                 ]
