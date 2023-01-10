9b25833f084c8a1a70be5e264c020726b25a18e9
Mala Patke <alis@khanlabschool.org>
2023-01-10 14:52:53 -0800

    Let's see if this one does anything

--- client-side-code/beatPage.js
+++ client-side-code/beatPage.js
@@ -3,8 +3,11 @@ function createNewListElement(title, text, url, imgLink = "https://i.imgur.com/y
     let listElement = document.getElementsByClassName("list-item")[0].cloneNode(true);
 
     //New image
-    listElement.childNodes[1].childNodes[1].childNodes[1].attributes["data-src"].nodeValue = imgLink;
-    listElement.childNodes[1].childNodes[1].childNodes[1].attributes["data-image"].nodeValue = imgLink;
+    let img = document.createElement('img');
+    img.src = imgLink;
+    img.style = "width: 100%; object-fit: cover;";
+    img.classList.add('list-image');
+    listElement.childNodes[1].childNodes[1].childNodes[1].replaceWith(img.cloneNode(true));
 
     //New header
     listElement.childNodes[3].childNodes[1].childNodes[1].textContent = title;
@@ -21,12 +24,12 @@ function createNewListElement(title, text, url, imgLink = "https://i.imgur.com/y
 
 document.addEventListener("DOMContentLoaded", () => {
     //This is manual rn but in the future it'll be automatic
-    let page = "opinion";
-    
+    let page = "opinion";    
     fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(`https://khranicle.vercel.app/api/getArticleData/?beat=${page}`)}`, {
         method: 'GET'
     }).then(res => res.json())
         .then(({ contents }) => {
-            for(let { title, desc, url, img } of JSON.parse(contents).reverse()) createNewListElement(title, desc, url, img)
-        })
+            for(let { title, desc, url, image } of JSON.parse(contents).reverse()) createNewListElement(title, desc, url, image);
+            document.getElementsByClassName("list-item")[0].remove();
+        });
 });
